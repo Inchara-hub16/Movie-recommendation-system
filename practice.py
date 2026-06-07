4e92404f3d3d47f961ec7d5bc19857a40bee0fee
Inchara Manojkumar <incharamanojkumar@Incharas-MacBook-Air.local>
2026-06-07 12:19:33 +0530

    Remove practice files

--- practice.py
+++
@@ -1,9 +0,0 @@
-def translation(phrase):
-    translation = ""
-    for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation += "g"
-        else:
-            translation += letter
-    return translation
-print(translation(input("enter the phrase: ")))
